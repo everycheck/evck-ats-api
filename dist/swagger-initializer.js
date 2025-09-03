8c06b225044939c3cf2a56c978365fde7c054d83
LudovicDebost <104353148+LudovicDebost@users.noreply.github.com>
2025-09-03 11:59:32 +0200

    Update doc (#1)
    * add QA server
    
    * update doc
    
    * update urls

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,20 +1,39 @@
-window.onload = function() {
+window.onload = function () {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "swagger.yaml",
-    dom_id: '#swagger-ui',
+    dom_id: "#swagger-ui",
     deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
+    persistAuthorization: true,
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [],
+    layout: "StandaloneLayout",
   });
 
   //</editor-fold>
+  //
+  function waitForServerSelect(callback) {
+    const interval = setInterval(() => {
+      const serverSelect = document.querySelector("#servers");
+      if (serverSelect) {
+        clearInterval(interval);
+        callback(serverSelect);
+      }
+    }, 100);
+  }
+
+  // Restore the server selection if saved
+  waitForServerSelect((serverSelect) => {
+    const savedIndex = localStorage.getItem("swagger-selected-server-index");
+    if (savedIndex !== null && serverSelect.options.length > savedIndex) {
+      serverSelect.selectedIndex = savedIndex;
+      serverSelect.dispatchEvent(new Event("change", { bubbles: true }));
+    }
+
+    serverSelect.addEventListener("change", function () {
+      localStorage.setItem("swagger-selected-server-index", serverSelect.selectedIndex);
+    });
+  });
 };
